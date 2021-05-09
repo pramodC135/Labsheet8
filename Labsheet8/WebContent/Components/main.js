2198e977c692c3648b0081625db1b837cbd83aa3
pramodC135 <pramodmadhushan4@gmail.com>
2021-05-10 01:57:30 +0530

    com

--- Labsheet8/WebContent/Components/main.js
+++ Labsheet8/WebContent/Components/main.js
@@ -1,45 +1,6 @@
-
-// Fireup the plugins
-$(document).ready(function(){
-	
-	// initialise  slideshow
-	 $('.flexslider').flexslider({
-        animation: "slide",
-        start: function(slider){
-          $('body').removeClass('loading');
-        }
-      });
-
-});
 /**
- * Handles toggling the navigation menu for small screens.
+ * 
  */
-( function() {
-	var button = document.getElementById( 'topnav' ).getElementsByTagName( 'div' )[0],
-	    menu   = document.getElementById( 'topnav' ).getElementsByTagName( 'ul' )[0];
-
-	if ( undefined === button )
-		return false;
-
-	// Hide button if menu is missing or empty.
-	if ( undefined === menu || ! menu.childNodes.length ) {
-		button.style.display = 'none';
-		return false;
-	}
-
-	button.onclick = function() {
-		if ( -1 == menu.className.indexOf( 'srt-menu' ) )
-			menu.className = 'srt-menu';
-
-		if ( -1 != button.className.indexOf( 'toggled-on' ) ) {
-			button.className = button.className.replace( ' toggled-on', '' );
-			menu.className = menu.className.replace( ' toggled-on', '' );
-		} else {
-			button.className += ' toggled-on';
-			menu.className += ' toggled-on';
-		}
-	};
-} )();
 
 //page load
 $(document).ready(function()
@@ -107,8 +68,43 @@ function validateItemForm()
 	return true;
 }
 
-
-
-
-
-
+function getStudentCard(name, gender, year)
+{
+	var title = (gender == "Male") ? "Mr." : "Ms.";
+	
+	var yearNumber = "";
+	
+	switch (year)
+	{
+		case "1":
+		yearNumber = "1st";
+		break;
+		
+		case "2":
+		yearnumber = "2nd";
+		break;
+		
+		case "3":
+		yearNumber = "3rd";
+		break;
+		
+		case "4":
+		yearNumber = "4th";
+		break;
+	}
+	
+	var student = "";
+	//Generate card
+	
+	student += "<div class=\"student card bg-light m-2\" style=\"max-width: 10rem; float: left;\">";
+	
+	student += "<div class=\"card-body\">";
+	student += title + " " + name + " , ";
+	student += "<br>";
+	student += yearNumber  + "year";
+	student += "</div>";
+	student += "input type=\"button\" value=\"Remove\" class=\"btn btn-danger remove\">";
+	student += "</div>";
+	
+	return;
+}
